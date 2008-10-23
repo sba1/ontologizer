bb8322ef97f84243318b930694abac9b614ea43e
Sebastian Bauer <mail@sebastianbauer.info>
2008-10-23 11:45:49 +0000

    Started to display a graph for semantic similarity.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@20 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/calculation/SemanticResult.java
+++ ontologizer/src/ontologizer/calculation/SemanticResult.java
@@ -1,9 +1,14 @@
 package ontologizer.calculation;
 
 import ontologizer.ByteString;
+import ontologizer.association.AssociationContainer;
+import ontologizer.go.GOGraph;
 
 public class SemanticResult
 {
+	public GOGraph g;
+	public AssociationContainer assoc;
+
 	public ByteString [] names;
 	public double [][] mat;
 	public String name;
