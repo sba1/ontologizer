963a35bfda792ee45a14d452ceb336a49eeb368e
Sebastian Bauer <mail@sebastianbauer.info>
2008-10-23 10:25:51 +0000

    EnrichedGraphGenerationThread now inherits from NewGenerationGraphThread => less code.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@18 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/gui/swt/result/EnrichedGraphGenerationThread.java
+++ ontologizer/src/ontologizer/gui/swt/result/EnrichedGraphGenerationThread.java
@@ -1,6 +1,5 @@
 package ontologizer.gui.swt.result;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.util.HashSet;
 
@@ -9,109 +8,51 @@ import ontologizer.go.GOGraph;
 import ontologizer.go.Term;
 import ontologizer.go.TermID;
 import ontologizer.gui.swt.support.IGraphGenerationFinished;
+import ontologizer.gui.swt.support.IGraphGenerationSupport;
+import ontologizer.gui.swt.support.NewGraphGenerationThread;
 
 import org.eclipse.swt.widgets.Display;
 
 /**
  * Generates the graph by executing DOT. When finished
- * the finished method of the specifed constructor argument
+ * the finished method of the specified constructor argument
  * is executed in the context of the GUI thread.
  * 
  * @author Sebastian Bauer
  */
-public class EnrichedGraphGenerationThread extends Thread
+public class EnrichedGraphGenerationThread extends NewGraphGenerationThread
 {
 	public GOGraph go;
 	public Term emanatingTerm;
 	public double significanceLevel;
 	public HashSet<TermID> leafTerms = new HashSet<TermID>();
 	public EnrichedGOTermsResult result;
-	public Display display;
-	public String gfxOutFilename;
-	public String dotPath;
 
 	private IGraphGenerationFinished finished;
 
-	public EnrichedGraphGenerationThread(IGraphGenerationFinished finished)
+	private IGraphGenerationSupport support = new IGraphGenerationSupport()
 	{
-		setPriority(Thread.MIN_PRIORITY);
-
-		this.finished = finished;
-	}
-
-	public void run()
-	{
-		try
+		public void writeDOT(File dotFile)
 		{
-			final File dotTmpFile = File.createTempFile("onto", ".dot");
-			final File layoutedDotTmpFile = File.createTempFile("onto", ".dot");
-			final File gfxFile;
-
-			/* Remove temporary files on exit */
-			dotTmpFile.deleteOnExit();
-			layoutedDotTmpFile.deleteOnExit();
-
-			if (gfxOutFilename != null) gfxFile = new File(gfxOutFilename);
-			else gfxFile = null;
-
-			result.writeDOT( go.getGoTermContainer(), go, dotTmpFile,
+			result.writeDOT( go.getGoTermContainer(), go, dotFile,
 					significanceLevel, true,
 					emanatingTerm != null ? emanatingTerm.getID() : null,
 					leafTerms);
-
-			String [] args;
-			if (gfxFile != null)
-			{
-				String gfxOption = "-Tpng";
-
-				if (gfxFile.getName().endsWith(".svg"))
-					gfxOption = "-Tsvg";
-				if (gfxFile.getName().endsWith(".dot"))
-					gfxOption = "-Tdot";
-				if (gfxFile.getName().endsWith(".ps"))
-					gfxOption = "-Tps2";
-
-				args = new String[]{
-						dotPath, dotTmpFile.getCanonicalPath(),
-						gfxOption, "-o", gfxFile.getCanonicalPath(),
-						"-Tdot", "-o", layoutedDotTmpFile.getCanonicalPath()};
-			} else
-			{
-				args = new String[]{
-						dotPath, dotTmpFile.getCanonicalPath(),
-						"-Tdot", "-o", layoutedDotTmpFile.getCanonicalPath()};
-			}
-
-			Process dotProcess = Runtime.getRuntime().exec(args);
-
-			int c;
-			BufferedInputStream es = new BufferedInputStream(dotProcess.getErrorStream());
-			StringBuilder errStr = new StringBuilder();
-			while ((c = es.read()) != -1)
-				errStr.append((char) c);
-
-			dotProcess.waitFor();
-
-			final boolean success = dotProcess.exitValue() == 0;
-
-			System.err.println(errStr.toString());
-
-			/* Issue finished message on GUI thread context */
-			display.syncExec(new Runnable()
-			{
-				public void run()
-				{
-					finished.finished(success,"Dot returned a failure!",gfxFile,layoutedDotTmpFile);
-				}
-			});
-		} catch (final Exception e)
-		{
-			/* Issue finished message on GUI thread context */
-			display.syncExec(new Runnable(){
-				public void run()
-				{
-					finished.finished(false,e.getLocalizedMessage(),null,null);
-				}});
 		}
+		
+		public void layoutFinished(boolean success, String msg, File pngFile,
+				File dotFile)
+		{
+			finished.finished(success, msg, pngFile, dotFile);
+		}
+	};
+
+	public EnrichedGraphGenerationThread(Display display, String dotCMDPath, IGraphGenerationFinished finished)
+	{
+		super(display, dotCMDPath);
+		
+		setSupport(support);
+
+		this.finished = finished;
 	}
 };
