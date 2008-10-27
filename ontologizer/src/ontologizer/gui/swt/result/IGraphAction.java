8a8d334617bb12751a4fb8a62b9cad1290b74932
Sebastian Bauer <mail@sebastianbauer.info>
2008-10-27 14:16:53 +0000

    Added comments.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@34 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/gui/swt/result/IGraphAction.java
+++ ontologizer/src/ontologizer/gui/swt/result/IGraphAction.java
@@ -1,9 +1,31 @@
 package ontologizer.gui.swt.result;
 
+/**
+ * Interface for the graphical display.
+ * 
+ * @author Sebastian Bauer
+ */
 public interface IGraphAction
 {
+	/**
+	 * Should the displayed graphics be fitted to the actual dimensions.
+	 *  
+	 * @param fit
+	 */
 	public void setScaleToFit(boolean fit);
+	
+	/**
+	 * Zoom in.
+	 */
 	public void zoomIn();
+	
+	/**
+	 * Zoom out.
+	 */
 	public void zoomOut();
+	
+	/**
+	 * Reset zoom.
+	 */
 	public void resetZoom();
 }
