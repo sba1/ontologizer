49b3f0d30b427880d355da3e40c3558e07adbb65
Sebastian Bauer <mail@sebastianbauer.info>
2008-12-19 12:39:24 +0000

    Added some more tests.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@115 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/go/TermIDTest.java
+++ ontologizer/src/ontologizer/go/TermIDTest.java
@@ -13,16 +13,20 @@ public class TermIDTest extends TestCase
 		TermID gid5 = new TermID(0352623);
 		TermID gid6 = new TermID(0120211);
 		TermID gid7 = new TermID(120211);
+		TermID gid8 = new TermID("HPO:0120211");
+		TermID gid9 = new TermID(new Prefix("GO"),120211);
 
 		Assert.assertTrue(gid.equals(gid2));
 		Assert.assertFalse(gid.equals(gid3));
 		Assert.assertTrue(gid.equals(gid5));  // yes, oktal
 		Assert.assertFalse(gid.equals(gid6)); // no, oktal
 		Assert.assertTrue(gid.equals(gid7));  // yes, decimal
+		Assert.assertFalse(gid.equals(gid8)); // no, different prefix
+		Assert.assertTrue(gid.equals(gid9));  // yes, uses explicit prefix construction
 
 		try
 		{
-			@SuppressWarnings("unused") TermID gid4 = new TermID("GO:01202111");
+			new TermID("GO:01202111");
 			Assert.assertTrue(false);
 		} catch(IllegalArgumentException ex)
 		{
