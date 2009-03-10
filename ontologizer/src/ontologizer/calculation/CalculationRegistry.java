1dc70f7a0d9d6b8f1f5b39c740709f53f047094f
Sebastian Bauer <mail@sebastianbauer.info>
2009-03-10 08:07:39 +0000

    The probabilistic method shouldn't be registered.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@123 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/calculation/CalculationRegistry.java
+++ ontologizer/src/ontologizer/calculation/CalculationRegistry.java
@@ -86,7 +86,7 @@ public class CalculationRegistry
 		registerCalculation(new ParentChildCutCalculation());
 		registerCalculation(new TopCalculation());
 		registerCalculation(new TopologyWeightedCalculation());
-		registerCalculation(new ProbabilisticCalculation());
+//		registerCalculation(new ProbabilisticCalculation());
 //		registerCalculation(new NewApproachCalculation());
 //		registerCalculation(new ChiSquareQCalculation());
 	}
