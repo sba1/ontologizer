c988f07e061c0e7d4a9a4b660444efa26366547c
Sebastian Bauer <mail@sebastianbauer.info>
2009-07-13 09:49:52 +0000

    Restricted the terms to terms of the study set.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@140 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/calculation/TopologyWeightedCalculation.java
+++ ontologizer/src/ontologizer/calculation/TopologyWeightedCalculation.java
@@ -178,7 +178,8 @@ public class TopologyWeightedCalculation extends AbstractHypergeometricCalculati
 
 		GOTermEnumerator studyTermEnumerator = studySet.enumerateGOTerms(graph,goAssociations);
 		GOTermEnumerator populationTermEnumerator = populationSet.enumerateGOTerms(graph,goAssociations);
-		Set<TermID> allAnnotatedTerms = populationTermEnumerator.getAllAnnotatedTermsAsSet();
+
+		Set<TermID> allAnnotatedTerms = studyTermEnumerator.getAllAnnotatedTermsAsSet();
 		GOLevels levels = graph.getGOLevels(allAnnotatedTerms);
 
 		for (int i=levels.getMaxLevel();i>=0;i--)
@@ -198,7 +199,6 @@ public class TopologyWeightedCalculation extends AbstractHypergeometricCalculati
 				computeTermSig(populationSet, studySet, graph, t, annotatedDescs, studySetResult, studyTermEnumerator, populationTermEnumerator);
 			}
 		}
-
 
 		return studySetResult;
 	}
