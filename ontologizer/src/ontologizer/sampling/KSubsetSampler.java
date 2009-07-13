17bb20a59e48d551afef99bef59f404d56afe842
Sebastian Bauer <mail@sebastianbauer.info>
2009-07-13 09:50:21 +0000

    Minor changes.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@141 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/sampling/KSubsetSampler.java
+++ ontologizer/src/ontologizer/sampling/KSubsetSampler.java
@@ -1,7 +1,9 @@
 package ontologizer.sampling;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Random;
 
 import ontologizer.Hypergeometric;
 
@@ -30,6 +32,8 @@ public class KSubsetSampler<T>
 	private int nObj;
 
 	private Hypergeometric hyperg;
+	
+	private Random rnd;
 
 	/**
 	 * The logarithm of the p-value 0.5, needed below
@@ -42,9 +46,8 @@ public class KSubsetSampler<T>
 	 * 
 	 * @param objects
 	 */
-	public KSubsetSampler(Iterable<T> objects)
+	public KSubsetSampler(Iterable<T> object)
 	{
-		super();
 		this.objects = new ArrayList<T>();
 		for (T obj : objects)
 		{
@@ -53,8 +56,23 @@ public class KSubsetSampler<T>
 		// this.objects.addAll(objects);
 		this.nObj = this.objects.size();
 		this.hyperg = new Hypergeometric();
+		this.rnd = new Random();
 	}
 
+	/**
+	 * Constructor. 
+	 * 
+	 * @param coll
+	 * @param rnd object that is used to generate random values.
+	 */
+	public KSubsetSampler(Collection<T> coll, Random rnd)
+	{
+		this.objects = new ArrayList<T>(coll);
+		this.nObj = coll.size();
+		this.hyperg = new Hypergeometric();
+		this.rnd = rnd;
+	}
+	
 	/**
 	 * Creates one random k-subset of the object set.
 	 * 
@@ -67,13 +85,21 @@ public class KSubsetSampler<T>
 		ArrayList<T> sample = new ArrayList<T>();
 
 		k = Math.min(k, this.nObj);
+		
+		if (k == this.nObj)
+		{
+			sample.addAll(this.objects);
+			return sample;
+		}
 
 		for (int i = this.nObj - 1; i >= this.nObj - k; i--)
 		{
-			int index = (int) (Math.random() * i);
-			sample.add(objects.get(index));
-			objects.add(index, objects.remove(i));
-			objects.add(i, objects.remove(index + 1));
+			int choose = rnd.nextInt(i);
+
+			T item = objects.get(choose);
+			sample.add(item);
+			objects.set(choose, objects.get(i));
+			objects.set(i, item);
 		}
 
 		return sample;
