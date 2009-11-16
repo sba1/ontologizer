2a641949b72d177a3ad30d74a47e6a0acd6fcd63
Sebastian Bauer <mail@sebastianbauer.info>
2009-11-16 16:24:23 +0000

    r194@jasper:  sba | 2009-08-25 16:01:49 +0200  Added default constructor.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@201 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/PopulationSet.java
+++ ontologizer/src/ontologizer/PopulationSet.java
@@ -40,6 +40,14 @@ public class PopulationSet extends StudySet
 	}
 
 	/**
+	 * Constructs the population set.
+	 */
+	public PopulationSet()
+	{
+		super();
+	}
+
+	/**
 	 * constructs an empty PopulationSet with the given name
 	 * 
 	 * @param name the name of the PopulationSet to construct
