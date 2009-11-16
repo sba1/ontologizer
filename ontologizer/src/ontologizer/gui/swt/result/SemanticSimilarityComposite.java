54322326ff73375694471cc6f04240faeb7f11c7
Sebastian Bauer <mail@sebastianbauer.info>
2009-11-16 16:46:24 +0000

    r246@jasper:  sba | 2009-10-13 14:54:38 +0200  Added preliminary support for latex documents.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@253 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/gui/swt/result/SemanticSimilarityComposite.java
+++ ontologizer/src/ontologizer/gui/swt/result/SemanticSimilarityComposite.java
@@ -408,6 +408,15 @@ public class SemanticSimilarityComposite extends Composite implements IGraphActi
 		File tableFile = new File(path);
 		result.writeTable(tableFile);
 	}
+	
+	public void latexSave(String path)
+	{
+		MessageBox mbox = new MessageBox(getShell(), SWT.ICON_ERROR | SWT.OK);
+		mbox.setMessage("Storing results of a semantic similarity\nanalsis is not supported");
+		mbox.setText("Ontologizer - Error");
+		mbox.open();
+
+	}
 
 	public void saveGraph(String file)
 	{
