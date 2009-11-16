1c9454420c54d36e82da086b183d7195af87c32c
Sebastian Bauer <mail@sebastianbauer.info>
2009-11-16 16:23:17 +0000

    r192@jasper:  sba | 2009-08-25 15:28:37 +0200  Moved private classes into an own class file.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@199 07a4415e-894b-0410-a0d6-e7c924882cef

---
+++ ontologizer/src/ontologizer/calculation/b2g/DoubleParam.java
@@ -0,0 +1,47 @@
+package ontologizer.calculation.b2g;
+
+import ontologizer.calculation.b2g.B2GParam.Type;
+
+class DoubleParam extends B2GParam
+{
+	private double val;
+
+	public DoubleParam(Type type, double val)
+	{
+		super(type);
+
+		this.val = val;
+	}
+
+	public DoubleParam(DoubleParam p)
+	{
+		super(p);
+
+		this.val = p.val;
+	}
+
+	public DoubleParam(Type type)
+	{
+		super(type);
+
+		if (type == Type.FIXED) throw new IllegalArgumentException("Parameter could not be instanciated of type Fixed.");
+	}
+
+	double getValue()
+	{
+		return val;
+	}
+
+	void setValue(double newVal)
+	{
+		this.val = newVal;
+		setType(Type.FIXED);
+	}
+
+	@Override
+	public String toString()
+	{
+		if (isFixed()) return String.format("%g",val);
+		return getType().toString();
+	}
+}
---
+++ ontologizer/src/ontologizer/calculation/b2g/VariableAlphaBetaScore.java
@@ -0,0 +1,111 @@
+package ontologizer.calculation.b2g;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import ontologizer.ByteString;
+import ontologizer.GOTermEnumerator;
+import ontologizer.go.TermID;
+
+/**
+ * Score of a setting in which alpha and beta are known.
+ *
+ * @author Sebastian Bauer
+ */
+class VariableAlphaBetaScore extends Bayes2GOScore
+{
+	private HashMap<ByteString, Double> llr = new HashMap<ByteString,Double>();
+	private double alpha;
+	private double beta;
+
+	private double score;
+
+	public VariableAlphaBetaScore(Random rnd, List<TermID> termList, GOTermEnumerator populationEnumerator, Set<ByteString> observedActiveGenes, double alpha, double beta)
+	{
+		super(rnd, termList, populationEnumerator, observedActiveGenes);
+
+		this.alpha = alpha;
+		this.beta = beta;
+
+		calcLLR();
+	}
+
+	public void calcLLR()
+	{
+		for (ByteString g : population)
+		{
+			if (observedActiveGenes.contains(g))
+				llr.put(g, Math.log(1-beta) - Math.log(alpha)); // P(oi=1|h=1) / P(oi=1|h=0)
+			else
+				llr.put(g, Math.log(beta) - Math.log(1-alpha)); // P(oi=0|h=1) / P(oi=0|h=0)
+		}
+
+	}
+
+	private int proposalSwitch;
+	private TermID proposalT1;
+	private TermID proposalT2;
+
+	public void hiddenGeneActivated(ByteString gene)
+	{
+		score += llr.get(gene);
+	}
+
+	public void hiddenGeneDeactivated(ByteString gene)
+	{
+		score -= llr.get(gene);
+	}
+
+	@Override
+	public void proposeNewState(long rand)
+	{
+		long oldPossibilities = getNeighborhoodSize();
+
+		proposalSwitch = -1;
+		proposalT1 = null;
+		proposalT2 = null;
+
+		long choose = Math.abs(rand) % oldPossibilities;
+
+		if (choose < termsArray.length)
+		{
+			/* on/off */
+			proposalSwitch = (int)choose;
+			switchState(proposalSwitch);
+		}	else
+		{
+			long base = choose - termsArray.length;
+
+			int activeTermPos = (int)(base / numInactiveTerms);
+			int inactiveTermPos = (int)(base % numInactiveTerms);
+
+			for (TermID tid : activeTerms)
+				if (activeTermPos-- == 0) proposalT1 = tid;
+			proposalT2 = inactiveTermsArray[inactiveTermPos];
+
+			exchange(proposalT1, proposalT2);
+		}
+	}
+
+	@Override
+	public double getScore()
+	{
+		return score + activeTerms.size() * Math.log(p/(1.0-p));
+	}
+
+	@Override
+	public void undoProposal()
+	{
+		if (proposalSwitch != -1)	switchState(proposalSwitch);
+		else exchange(proposalT2, proposalT1);
+	}
+
+	public long getNeighborhoodSize()
+	{
+		return termsArray.length + activeTerms.size() * numInactiveTerms;
+	}
+
+}
+
