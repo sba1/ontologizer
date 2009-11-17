7ad6a7549952797bc693ee1ad0d5112bc5d9195e
Sebastian Bauer <mail@sebastianbauer.info>
2009-11-17 16:26:24 +0000

    Test with svn:ignore.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@330 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/BuildInfo.java
+++ ontologizer/src/ontologizer/BuildInfo.java
@@ -2,7 +2,7 @@
 package ontologizer;
 
 public class BuildInfo {
-	public static final String revisionNumber="328";
+	public static final String revisionNumber="329";
 	public static final String date ="20091117";
 }
 		
