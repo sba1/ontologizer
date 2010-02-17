ae584d7df51b0a52798d8bc1ac40ed66ffa2cf15
Sebastian Bauer <mail@sebastianbauer.info>
2010-02-17 15:49:16 +0000

    Incremental check-in.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@356 07a4415e-894b-0410-a0d6-e7c924882cef

---
+++ branches/ontologizer/rcp-development/src/ontologizer/rcp/editors/ItemEditor.java
@@ -0,0 +1,14 @@
+package ontologizer.rcp.editors;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.part.EditorPart;
+
+public class ItemEditor extends TextEditor {
+
+
+}
