f0553b65ce2079b480cfb3835b8d4ebf29537d7d
Sebastian Bauer <mail@sebastianbauer.info>
2010-03-10 11:26:04 +0000

    Updated.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@365 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/BuildInfo.java
+++ ontologizer/src/ontologizer/BuildInfo.java
@@ -2,7 +2,7 @@
 package ontologizer;
 
 public class BuildInfo {
-	public static final String revisionNumber="329";
-	public static final String date ="20091117";
+	public static final String revisionNumber="351";
+	public static final String date ="20100310";
 }
 		
