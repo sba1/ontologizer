507ccba2d585db37db1fe6898c015c4f57ca32fd
Sebastian Bauer <mail@sebastianbauer.info>
2010-06-24 09:46:08 +0000

    More refactorization regarding the dot attributes provider.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@452 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/gui/swt/GraphWindow.java
+++ ontologizer/src/ontologizer/gui/swt/GraphWindow.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 import java.util.Set;
 
 import ontologizer.GlobalPreferences;
-import ontologizer.IDotAttributesProvider;
+import ontologizer.AbstractDotAttributesProvider;
 import ontologizer.go.GOGraph;
 import ontologizer.go.Namespace;
 import ontologizer.go.Term;
@@ -77,7 +77,7 @@ class GraphWindow extends ApplicationWindow
 				}
 	
 			}
-		}, new IDotAttributesProvider()
+		}, new AbstractDotAttributesProvider()
 		{
 
 			public String getDotNodeAttributes(TermID id)
