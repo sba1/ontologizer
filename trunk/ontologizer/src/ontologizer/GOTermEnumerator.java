e53781af8341dc4e7578a848a7be2674a8216ce5
Sebastian Bauer <mail@sebastianbauer.info>
2011-06-30 14:06:03 +0000

    Removed redundant import statement.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@560 07a4415e-894b-0410-a0d6-e7c924882cef

--- trunk/ontologizer/src/ontologizer/GOTermEnumerator.java
+++ trunk/ontologizer/src/ontologizer/GOTermEnumerator.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.Map.Entry;
