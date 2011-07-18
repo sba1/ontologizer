59ad99a33a3c7793f9c868bee9172b3875282a23
Sebastian Bauer <mail@sebastianbauer.info>
2011-07-18 14:01:42 +0000

    Write out the table in a sorted manner.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@573 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/calculation/EnrichedGOTermsResult.java
+++ ontologizer/src/ontologizer/calculation/EnrichedGOTermsResult.java
@@ -3,7 +3,9 @@ package ontologizer.calculation;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -115,8 +117,14 @@ public class EnrichedGOTermsResult extends AbstractGOTermsResult
 
 			out.write(first.propHeaderToString());
 
-			/* Write out table contents */
+			/* Place the result into an own list, so we can sort the results */
+			ArrayList<AbstractGOTermProperties> propsList = new ArrayList<AbstractGOTermProperties>();
 			for (AbstractGOTermProperties props : this)
+				propsList.add(props);
+			Collections.sort(propsList);
+
+			/* Write out table contents */
+			for (AbstractGOTermProperties props : propsList)
 			{
 				out.println(props.propLineToString(populationGeneCount,
 						studyGeneCount));
