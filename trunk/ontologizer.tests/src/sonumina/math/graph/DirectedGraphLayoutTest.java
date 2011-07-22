763aa8e655c9b9773750162dbaf1866e2e242d75
Sebastian Bauer <mail@sebastianbauer.info>
2011-07-22 14:21:51 +0000

    Implemented a very naively implemented greedy optimization procedure for layouting the graph.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@578 07a4415e-894b-0410-a0d6-e7c924882cef

---
+++ trunk/ontologizer.tests/src/sonumina/math/graph/DirectedGraphLayoutTest.java
@@ -0,0 +1,57 @@
+package sonumina.math.graph;
+
+import sonumina.math.graph.DirectedGraphLayout.Dimension;
+import junit.framework.TestCase;
+
+public class DirectedGraphLayoutTest extends TestCase
+{
+
+	public void testLayout()
+	{
+		final String [] names = new String[]{
+				"|Root|","|A|", "|A third node, which is very long|", "|4th|", "|5th|"
+		};
+		DirectedGraph<Integer> graph = new DirectedGraph<Integer>();
+		graph.addVertex(0);
+		graph.addVertex(1);
+		graph.addVertex(2);
+		graph.addVertex(3);
+		graph.addVertex(4);
+		
+		graph.addEdge(new Edge<Integer>(0,2));
+		graph.addEdge(new Edge<Integer>(1,2));
+		graph.addEdge(new Edge<Integer>(2,3));
+		graph.addEdge(new Edge<Integer>(2,4));
+
+		final StringBuilder str = new StringBuilder();
+		final String emptyLine = "                                                                                            \n";
+		for (int i=0;i<6;i++)
+			str.append(emptyLine);
+
+		DirectedGraphLayout.layout(graph, new DirectedGraphLayout.IGetDimension<Integer>()
+				{
+
+					@Override
+					public void get(Integer vertex, Dimension d)
+					{
+						String name = names[vertex]; 
+						d.height = 1;
+						d.width = name.length();
+					}
+			
+				}, new DirectedGraphLayout.IPosition<Integer>() {
+					@Override
+					public void set(Integer vertex, int left, int top)
+					{
+						String name = names[vertex];
+						System.out.println(vertex + " at " + left + "/" + top);
+						int start = left + top * emptyLine.length();
+						str.replace(start, start + name.length(), name);
+					}
+				});
+
+		
+		System.out.println(str.toString());
+		
+	}
+}
