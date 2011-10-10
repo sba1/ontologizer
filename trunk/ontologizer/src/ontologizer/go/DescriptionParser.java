2705a524927817749e3a0fde2221389d5ef44817
Sebastian Bauer <mail@sebastianbauer.info>
2011-10-10 14:58:00 +0000

    Added an implementation of the DescriptionParser and modified the test.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@606 07a4415e-894b-0410-a0d6-e7c924882cef

--- trunk/ontologizer/src/ontologizer/go/DescriptionParser.java
+++ trunk/ontologizer/src/ontologizer/go/DescriptionParser.java
@@ -1,5 +1,8 @@
 package ontologizer.go;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * This is a simple parser for descriptions used in OBO.
  *
@@ -7,9 +10,12 @@ package ontologizer.go;
  */
 public class DescriptionParser
 {
+	private static final Pattern termPattern = Pattern.compile("`([^`]*)` \\(([^)]*)\\)");
+	private static final Pattern textPattern = Pattern.compile("([^`]+)`");
+
 	public interface IDescriptionPartCallback
 	{
-		public boolean part(String txt, TermID tid);
+		public boolean part(String txt, String ref);
 	}
 
 	/**
@@ -21,6 +27,33 @@ public class DescriptionParser
 	 */
 	public static void parse(String txt, IDescriptionPartCallback cb)
 	{
+		int pos = 0;
+
+		while (pos < txt.length())
+		{
+			String ptxt = txt.substring(pos);
+
+			Matcher m = termPattern.matcher(ptxt);
+			if (m.find() && m.start() == 0)
+			{
+				pos += m.end();
+				cb.part(m.group(1),m.group(2));
+			} else
+			{
+				m = textPattern.matcher(ptxt);
+				if (m.find())
+				{
+					cb.part(m.group(1), null);
+					pos += m.end() - 1; /* The ` is important for the next round, so enure that we start with that */
+				}
+				else
+				{
+					cb.part(ptxt,null);
+					return;
+				}
+
+			}
+		}
 
 	}
 }
