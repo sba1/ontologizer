0c265c42adb1feb6ee9b9a72556c56d966d366d5
Sebastian Bauer <mail@sebastianbauer.info>
2011-10-10 14:02:50 +0000

    Added not yet implemented description parser and a test case.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@605 07a4415e-894b-0410-a0d6-e7c924882cef

---
+++ trunk/ontologizer/src/ontologizer/go/DescriptionParser.java
@@ -0,0 +1,26 @@
+package ontologizer.go;
+
+/**
+ * This is a simple parser for descriptions used in OBO.
+ * 
+ * @author Sebastian Bauer
+ */
+public class DescriptionParser
+{
+	public interface IDescriptionPartCallback
+	{
+		public boolean part(String txt, TermID tid);
+	}
+	
+	/**
+	 * Parse the given description. For each component, the part() method
+	 * of IDescriptionPartCallback is called with appropriate parameters.
+	 * 
+	 * @param txt text to be parsed.
+	 * @param cb callback to be called
+	 */
+	public static void parse(String txt, IDescriptionPartCallback cb)
+	{
+		
+	}
+}
