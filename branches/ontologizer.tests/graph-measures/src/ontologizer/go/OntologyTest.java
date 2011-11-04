a0ee555e664d8976b56f07afdf9994b6db01d4a4
Sebastian Bauer <mail@sebastianbauer.info>
2011-11-04 10:56:33 +0000

    Branched test for graph measures.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@629 07a4415e-894b-0410-a0d6-e7c924882cef

---
+++ branches/ontologizer.tests/graph-measures/src/ontologizer/go/OntologyTest.java
@@ -0,0 +1,109 @@
+package ontologizer.go;
+
+import java.util.Set;
+
+import ontologizer.go.Ontology.IVisitingGOVertex;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class OntologyTest extends TestCase
+{
+	private TermContainer goTerms;
+
+	private Ontology graph;
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		String GOtermsOBOFile = "data/GO_test.obo";
+
+		/* Parse file and create term container */
+		System.out.println("Parse OBO file");
+		OBOParser oboParser = new OBOParser(GOtermsOBOFile);
+		System.out.println(oboParser.doParse());
+		goTerms = new TermContainer(oboParser.getTermMap(), oboParser.getFormatVersion(), oboParser.getDate());
+
+		/* Build graph */
+		System.out.println("Build graph");
+		graph = new Ontology(goTerms);
+	}
+
+	public void testRoot()
+	{
+		Set<String> terms;
+
+		terms = graph.getTermChildrenAsStrings("GO:0000000");
+		Assert.assertTrue("Test we get some Set<String> object back", terms != null);
+		Assert.assertTrue("Root has three descendants", terms.size() == 3);
+
+		terms = graph.getTermParentsAsStrings("GO:0000000");
+		Assert.assertTrue("Test we gat some Set<String> object back", terms != null);
+		Assert.assertTrue("Root has no ancestors", terms.size() == 0);
+	}
+
+	public void testExistsPath()
+	{
+		Assert.assertTrue(graph.existsPath(new TermID("GO:0009987"),
+				new TermID("GO:0006281")));
+		Assert.assertFalse(graph.existsPath(new TermID("GO:0006281"),
+				new TermID("GO:0009987")));
+
+		Assert.assertTrue(graph.existsPath(new TermID("GO:0008150"),
+				new TermID("GO:0006281")));
+		Assert.assertFalse(graph.existsPath(new TermID("GO:0006281"),
+				new TermID("GO:0008150")));
+
+		Assert.assertFalse(graph.existsPath(new TermID("GO:0006139"),
+				new TermID("GO:0009719")));
+		Assert.assertFalse(graph.existsPath(new TermID("GO:0009719"),
+				new TermID("GO:0006139")));
+	}
+
+	public void testWalkToRoot()
+	{
+		/**
+		 * A basic visitor: It simply counts up the number of visisted terms.
+		 *
+		 * @author Sebastian Bauer
+		 */
+		class VisitingGOVertex implements IVisitingGOVertex
+		{
+			public int count = 0;
+
+			public boolean visited(Term term)
+			{
+				count++;
+				return true;
+			}
+
+			public void resetCount()
+			{
+				count = 0;
+			};
+
+			public int getCount()
+			{
+				return count;
+			};
+		}
+		;
+
+		VisitingGOVertex vistingGOVertex = new VisitingGOVertex();
+
+		/*
+		 * Note, if GO changes these values are no longer correct. But you can
+		 * verfiy them then via www.godatabase.org.
+		 */
+		graph.walkToSource(new TermID("GO:0008152"), vistingGOVertex);
+		Assert.assertTrue(vistingGOVertex.getCount() == 4);
+		vistingGOVertex.resetCount();
+
+		graph.walkToSource(new TermID("GO:0044237"), vistingGOVertex);
+		Assert.assertTrue(vistingGOVertex.getCount() == 7);
+		vistingGOVertex.resetCount();
+
+		graph.walkToSource(new TermID("GO:0006281"), vistingGOVertex);
+		Assert.assertTrue(vistingGOVertex.getCount() == 17);
+	}
+}
