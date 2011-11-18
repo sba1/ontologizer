3d133b677d003da2b2ce55f8132c296b70c34d13
Martin Wettig <Martin.Wettig@charite.de>
2011-11-18 11:48:58 +0000

    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@646 07a4415e-894b-0410-a0d6-e7c924882cef

--- branches/ontologizer/graph-measures/src/sonumina/math/graph/DirectedGraph.java
+++ branches/ontologizer/graph-measures/src/sonumina/math/graph/DirectedGraph.java
@@ -1138,8 +1138,44 @@ public class DirectedGraph<VertexType> extends AbstractGraph<VertexType> impleme
 	 */
 	public HashSet<VertexType> getSharedNeighbours(VertexType m, VertexType n)
 	{
+		HashMap<Integer, VertexType> index2Vertex = new HashMap<Integer, VertexType>();
 		if(slimGraph == null)
+		{
+			/*int idx = 0;
+
+			HashMap<VertexType,Integer> vertex2Index = new HashMap<VertexType, Integer>();
+			int[][] vertexChildren = new int[getNumberOfVertices()][];
+
+			for(VertexType v : getVertices())
+			{
+				vertex2Index.put(v, idx);
+				index2Vertex.put(idx,v);
+				++idx;
+			}
+			
+			ArrayList<Integer> children = new ArrayList<Integer>();
+			for(VertexType v : getVertices())
+			{
+				Iterator<VertexType> iter = getChildNodes(v);
+				while(iter.hasNext())
+					children.add(vertex2Index.get(iter.next()));
+				
+				int[] tmp = new int[children.size()];
+				
+				for(int i = 0; i < children.size(); i++)
+					tmp[i] = children.get(i);
+				
+				Arrays.sort(tmp);
+				vertexChildren[vertex2Index.get(v)] = tmp;
+				children.clear();
+			}
+			
+			slimGraph = new SlimDirectedGraphView<VertexType>( new DirectedGraph<VertexType>() );
+			slimGraph.vertex2Index = vertex2Index;
+			slimGraph.vertexChildren = vertexChildren;*/
+			
 			slimGraph = new SlimDirectedGraphView<VertexType>(this);
+		}
 		
 		int mIndex = slimGraph.getVertexIndex(m);
 		int nIndex = slimGraph.getVertexIndex(n);
@@ -1152,7 +1188,7 @@ public class DirectedGraph<VertexType> extends AbstractGraph<VertexType> impleme
 		int mPtr = 0;
 		int nPtr = 0;
 		HashSet<VertexType> sN = new HashSet<VertexType>();
-		while(mPtr < mChildren.length && nPtr < nChildren.length) //probably an endless loop; Index Out of Range
+		while(mPtr < mChildren.length && nPtr < nChildren.length)
 		{
 			if(mChildren[mPtr] < nChildren[nPtr])
 				++mPtr;
@@ -1160,7 +1196,7 @@ public class DirectedGraph<VertexType> extends AbstractGraph<VertexType> impleme
 				++nPtr;
 			else
 			{
-				sN.add(slimGraph.getVertex(mChildren[mPtr]));
+				sN.add(index2Vertex.get(mChildren[mPtr]));
 				++mPtr;
 				++nPtr;
 			}
