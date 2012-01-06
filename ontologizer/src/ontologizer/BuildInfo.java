788489c7752c656e4658d02389fb6d96c2591945
Sebastian Bauer <mail@sebastianbauer.info>
2012-01-06 12:14:14 +0000

    The build details are now are assumed to be stored in the class BuildInfoData. This information is extracted now from BuildInfo using reflections.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@692 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/BuildInfo.java
+++ ontologizer/src/ontologizer/BuildInfo.java
@@ -1,8 +1,78 @@
 
 package ontologizer;
 
-public class BuildInfo {
-	public static final String revisionNumber="623";
-	public static final String date ="20111215";
+import java.lang.reflect.Field;
+
+/**
+ * Simple class to get some info about the current
+ * Ontologizer instance. 
+ * 
+ * @author Sebastian Bauer
+ *
+ */
+public class BuildInfo
+{
+	private static String revisionNumber="NA";
+	private static String date ="NA";
+	private static String version = "2.0";
+	private static boolean infoExtracted = false;
+
+
+	/**
+	 * Extract the info stored in BuildInfoData.class.
+	 */
+	private static void extractInfo()
+	{
+		if (infoExtracted)
+			return;
+		
+		try {
+			Class<?> c = Class.forName("ontologizer.BuildInfoData");
+			Field r = c.getField("revisionNumber");
+			Field d = c.getField("date");
+			revisionNumber = (String)r.get(null);
+			date = (String)d.get(null);
+		} catch (ClassNotFoundException e) {
+		} catch (SecurityException e) {
+		} catch (NoSuchFieldException e) {
+		} catch (IllegalArgumentException e) {
+		} catch (IllegalAccessException e) {
+		}
+		infoExtracted = true;
+	}
+	
+	/**
+	 * Returns the revision number.
+	 * 
+	 * @return
+	 */
+	public static String getRevisionNumber()
+	{
+		extractInfo();
+		
+		return revisionNumber;
+	}
+	
+	/**
+	 * Returns the compilation date.
+	 * 
+	 * @return
+	 */
+	public static String getDate()
+	{
+		extractInfo();
+
+		return date;
+	}
+	
+	/**
+	 * Returns the version string.
+	 * 
+	 * @return
+	 */
+	public static String getVersion()
+	{
+		return version;
+	}
 }
 		
