0e694effabd612aed8dc624c36d392a491ce0d0a
Sebastian Bauer <mail@sebastianbauer.info>
2012-02-08 16:47:42 +0000

    Changed keyword for identifying user logs.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@719 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer.tests/src/sonumina/math/changelog/BuildChangeLogTest.java
+++ ontologizer.tests/src/sonumina/math/changelog/BuildChangeLogTest.java
@@ -13,17 +13,17 @@ public class BuildChangeLogTest extends TestCase
 						"r716 | sba | 2012-02-08 16:30:24 +0100 (Wed, 08 Feb 2012) | 1 line\n" +
 						"\n" +
 						"Added support for arbitrary node paths.\n" +
-						"$userlog$Test\n"+
+						"$foruser$Test\n"+
 						"------------------------------------------------------------------------\n" +
 						"r715 | sba | 2012-02-08 14:51:00 +0100 (Wed, 08 Feb 2012) | 1 line\n" +
 						"\n" +
 						"Removed generateShuffledStudySets().\n" +
-						"$userlog$Test2\n"+
+						"$foruser$Test2\n"+
 						"------------------------------------------------------------------------\n" +
 						"r714 | sba | 2012-01-25 14:01:46 +0100 (Wed, 25 Jan 2012) | 1 line\n" +
 						"\n" +
 						"Added labels to the chart.\n" +
-						"$userlog$Test3\n"+
+						"$foruser$Test3\n"+
 						"------------------------------------------------------------------------\n";
 		Change [] result = BuildChangeLog.process(string);
 		Assert.assertEquals(3,result.length);
