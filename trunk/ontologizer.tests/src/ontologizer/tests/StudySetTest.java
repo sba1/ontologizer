0ab653b3ad368530588d21f6361b73a135b182df
Sebastian Bauer <mail@sebastianbauer.info>
2012-02-08 13:51:00 +0000

    Removed generateShuffledStudySets().
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@715 07a4415e-894b-0410-a0d6-e7c924882cef

--- trunk/ontologizer.tests/src/ontologizer/tests/StudySetTest.java
+++ trunk/ontologizer.tests/src/ontologizer/tests/StudySetTest.java
@@ -26,30 +26,4 @@ public class StudySetTest extends TestCase
 			Assert.assertTrue(randomStudy.getGeneCount() == 100);
 		}
 	}
-
-	public void testGenerateShuffledStudySets()
-	{
-		/* Note, this test fails currently!!! */
-		StudySet firstStudy = new StudySet("1st study");
-		StudySet secondStudy = new StudySet("2st study");
-
-		/* Generate the first study */
-		for (int i = 0;i<200;i++)
-			firstStudy.addGene(new ByteString("Gene"+i),"Description"+i);
-
-		/* Generate the second study (which overlaps with 1st study
-		 * in 100 genes and conists of 200 more genes) */
-		for (int i = 100;i<400;i++)
-			secondStudy.addGene(new ByteString("Gene"+i),"Description"+i);
-
-		StudySet array [] = new StudySet[]{firstStudy,secondStudy};
-
-		for (int i=0;i<100;i++)
-		{
-			StudySet shuffledArray [] = StudySet.generateShuffledStudySets(array);
-			Assert.assertEquals(200, shuffledArray[0].getGeneCount());
-			Assert.assertEquals(300, shuffledArray[1].getGeneCount());
-		}
-	}
-
 }
