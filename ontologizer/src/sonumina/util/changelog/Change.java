484a534243c558b9b4cd9f015992d885036b39cd
Sebastian Bauer <mail@sebastianbauer.info>
2012-02-08 16:45:46 +0000

    Started to implement a change log builder.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@717 07a4415e-894b-0410-a0d6-e7c924882cef

---
+++ ontologizer/src/sonumina/util/changelog/Change.java
@@ -0,0 +1,14 @@
+package sonumina.util.changelog;
+
+/**
+ * Represents a simple change.
+ * 
+ * @author Sebastian Bauer
+ */
+public class Change
+{
+	public String revision;
+	public String author;
+	public String date;
+	public String log;
+}
