f4c9d14a767d01ec396e99bd2e57a730348e3b46
Sebastian Bauer <mail@sebastianbauer.info>
2012-02-25 15:25:14 +0000

    Added method contains() and a test.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@750 07a4415e-894b-0410-a0d6-e7c924882cef

--- trunk/ontologizer/src/sonumina/collections/FullStringIndex.java
+++ trunk/ontologizer/src/sonumina/collections/FullStringIndex.java
@@ -1,6 +1,7 @@
 package sonumina.collections;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * A class mapping strings to other objects.
@@ -12,8 +13,41 @@ public class FullStringIndex<T>
 	private ArrayList<String> stringList = new ArrayList<String>();
 	private ArrayList<T> objectList = new ArrayList<T>();
 
+	private class StringIterator implements Iterator<T>
+	{
+		private String str;
+		private int pos = -1;
+
+		public StringIterator(String str)
+		{
+			this.str = str;
+		}
+
+		@Override
+		public boolean hasNext() {
+			while (true)
+			{
+				pos++;
+				if (pos >= stringList.size())
+					return false;
+				if (stringList.get(pos).contains(str))
+					return true;
+			}
+		}
+
+		@Override
+		public T next()
+		{
+			return objectList.get(pos);
+		}
+
+		@Override
+		public void remove() { }
+	}
+
 	/**
-	 * Associates the given string with the given object
+	 * Associates the given string with the given object.
+	 *
 	 * @param string
 	 * @param t
 	 */
@@ -32,5 +66,24 @@ public class FullStringIndex<T>
 	public int size()
 	{
 		return stringList.size();
+	}
+
+	/**
+	 * Returns an iterable which can be used to iterate over
+	 * elements that contain the given string.
+	 *
+	 * @param string
+	 * @return
+	 */
+	public Iterable<T> contains(final String string)
+	{
+		return new Iterable<T>()
+				{
+					@Override
+					public Iterator<T> iterator()
+					{
+						return new StringIterator(string);
+					}
+				};
 	}
 }
