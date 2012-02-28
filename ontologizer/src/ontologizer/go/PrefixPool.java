a512c853f8e87f66b1a065397b06a991e9962ca1
Sebastian Bauer <mail@sebastianbauer.info>
2012-02-28 10:23:40 +0000

    Refactored ReferencePool.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@769 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/go/PrefixPool.java
+++ ontologizer/src/ontologizer/go/PrefixPool.java
@@ -1,32 +1,6 @@
 package ontologizer.go;
 
-import java.util.HashMap;
-import java.util.HashSet;
-
-/**
- * A data structure to hold a pool of references.
- *
- * @author Sebastian Bauer
- *
- * @param <T>
- */
-class ReferencePool<T>
-{
-	/**
-	 * The container for all refs. Have to use a HashMap here,
-	 * as it is not possible to retrieve the reference
-	 * via a HashSet.
-	 */
-	private HashMap<T,T> referenceMap = new HashMap<T,T>();
-
-	public T map(T toBeMapped)
-	{
-		T ref = referenceMap.get(toBeMapped);
-		if (ref != null) return ref;
-		referenceMap.put(toBeMapped, toBeMapped);
-		return toBeMapped;
-	}
-}
+import sonumina.collections.ReferencePool;
 
 /**
  * A common pool for Prefix instances.
