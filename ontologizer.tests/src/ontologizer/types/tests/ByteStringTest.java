97b290c688cb800572b2a54266f31351c892921b
Sebastian Bauer <mail@sebastianbauer.info>
2012-02-29 16:03:04 +0000

    Added ability to convert a number from a ByteString. Made ByteString class final.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@787 07a4415e-894b-0410-a0d6-e7c924882cef

---
+++ ontologizer.tests/src/ontologizer/types/tests/ByteStringTest.java
@@ -0,0 +1,25 @@
+package ontologizer.types.tests;
+
+import ontologizer.types.ByteString;
+import junit.framework.TestCase;
+
+public class ByteStringTest extends TestCase
+{
+	public void testParseInteger()
+	{
+		assertEquals(1234, ByteString.parseFirstInt(new ByteString("1234")));
+		assertEquals(1234, ByteString.parseFirstInt(new ByteString("001234")));
+		assertEquals(4500, ByteString.parseFirstInt(new ByteString("4500")));
+		assertEquals(4500, ByteString.parseFirstInt(new ByteString("0000000004500")));
+		assertEquals(1234, ByteString.parseFirstInt(new ByteString("ssss1234ssss")));
+		assertEquals(1234, ByteString.parseFirstInt(new ByteString("ssss001234ssss")));
+		
+		try
+		{
+			ByteString.parseFirstInt(new ByteString("sswwscs"));
+			assertTrue(false);
+		} catch (NumberFormatException ex) {}
+		
+		
+	}
+}
