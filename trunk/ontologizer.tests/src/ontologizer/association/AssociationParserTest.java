f19b3e27a491cd0d783e349048956298c0e8dc34
Sebastian Bauer <mail@sebastianbauer.info>
2012-02-29 09:47:37 +0000

    The simple .ids file format no longer depends on the presence of the GoStats header.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@781 07a4415e-894b-0410-a0d6-e7c924882cef

--- trunk/ontologizer.tests/src/ontologizer/association/AssociationParserTest.java
+++ trunk/ontologizer.tests/src/ontologizer/association/AssociationParserTest.java
@@ -1,5 +1,8 @@
 package ontologizer.association;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import ontologizer.go.OBOParser;
@@ -27,5 +30,22 @@ public class AssociationParserTest extends TestCase
 		/* Note that this excludes NOT annotations */
 		a = ap.getAssociations().get(49088);
 		Assert.assertEquals("S000004009",a.getDB_Object().toString());
+	}
+
+	public void testIDS() throws IOException, OBOParserException
+	{
+		File tmp = File.createTempFile("test", ".ids");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(tmp));
+
+		bw.write("S000007287\tGO:0005763,GO:0032543,GO:0042255,GO:0003735,GO:0032543,GO:0005762,GO:0003735,GO:0003735,GO:0042255\n");
+		bw.write("S000004660\tGO:0005739,GO:0006810,GO:0005743,GO:0016020,GO:0055085,GO:0005488\n");
+		bw.write("S000004660\tGO:0006810,GO:0005471,GO:0016021,GO:0006783,GO:0005743,GO:0005743\n");
+		bw.flush();
+
+		OBOParser oboParser = new OBOParser(OBO_FILE);
+		oboParser.doParse();
+
+		AssociationParser ap = new AssociationParser(tmp.getAbsolutePath(),new TermContainer(oboParser.getTermMap(), "", ""));
+		Assert.assertEquals(21,ap.getAssociations().size());
 	}
 }
