7d72d0508a046872904339018a6be8c0f904e2e8
Sebastian Bauer <mail@sebastianbauer.info>
2012-08-01 10:09:17 +0000

    Small changes.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@855 07a4415e-894b-0410-a0d6-e7c924882cef

--- trunk/ontologizer.tests/src/ontologizer/go/OntologyTest.java
+++ trunk/ontologizer.tests/src/ontologizer/go/OntologyTest.java
@@ -35,7 +35,7 @@ public class OntologyTest extends TestCase
 
 		terms = graph.getTermChildrenAsStrings("GO:0000000");
 		Assert.assertTrue("Test we get some Set<String> object back", terms != null);
-		Assert.assertTrue("Root has three descendants", terms.size() == 3);
+		Assert.assertEquals("Root has three descendants",3, terms.size());
 
 		terms = graph.getTermParentsAsStrings("GO:0000000");
 		Assert.assertTrue("Test we gat some Set<String> object back", terms != null);
