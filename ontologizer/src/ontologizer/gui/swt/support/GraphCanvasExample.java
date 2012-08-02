ab5d5d8a46dbbb1a263ce8eb66708eeffd265b2c
Sebastian Bauer <mail@sebastianbauer.info>
2012-08-02 13:21:23 +0000

    Removed unused import.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@860 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer/src/ontologizer/gui/swt/support/GraphCanvasExample.java
+++ ontologizer/src/ontologizer/gui/swt/support/GraphCanvasExample.java
@@ -3,7 +3,6 @@ package ontologizer.gui.swt.support;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStreamWriter;
 
 import org.eclipse.swt.layout.FillLayout;
