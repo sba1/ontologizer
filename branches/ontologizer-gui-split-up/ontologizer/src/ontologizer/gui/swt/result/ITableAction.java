ae1aaad8d396ffe26ce7cd026ae74a8cf866f5b6
Sebastian Bauer <mail@sebastianbauer.info>
2012-10-07 18:42:12 +0000

    Deleted some packages that don't belong to the appropriate module and updated classpath and poms
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer@873 07a4415e-894b-0410-a0d6-e7c924882cef

--- branches/ontologizer-gui-split-up/ontologizer/src/ontologizer/gui/swt/result/ITableAction.java
+++
@@ -1,9 +0,0 @@
-package ontologizer.gui.swt.result;
-
-public interface ITableAction
-{
-	public void tableSave(String path);
-	public void latexSave(String path);
-	public void htmlSave(String path);
-	public void tableAnnotatedSetSave(String path);
-}
