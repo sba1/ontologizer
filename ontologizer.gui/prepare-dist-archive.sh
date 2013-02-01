05f3383305312d6146444ef90413be08a7b40125
Sebastian Bauer <mail@sebastianbauer.info>
2013-02-01 02:08:15 +0000

    Copy the ontologizer jar with dependencies.
    git-svn-id: https://ontologizer.svn.sourceforge.net/svnroot/ontologizer/trunk@913 07a4415e-894b-0410-a0d6-e7c924882cef

--- ontologizer.gui/prepare-dist-archive.sh
+++ ontologizer.gui/prepare-dist-archive.sh
@@ -14,6 +14,7 @@ cp -R ../ontologizer/local-maven-repo work/ontologizer
 find work/ontologizer | grep .svn$ | xargs rm -Rf 
 cp ../ontologizer/target/ontologizer-0.0.1-SNAPSHOT.jar  work/ontologizer/ontologizer.jar
 cp target/ontologizer.gui-0.0.1-SNAPSHOT.jar  work/ontologizer/ontologizer-gui.jar
+cp target/ontologizer.gui-0.0.1-SNAPSHOT-jar-with-dependencies.jar work/ontologizer/ontologizer-gui-with-dependencies.jar
 cp ontologizer.jnlp work/ontologizer
 find work/ontologizer | grep .jar$ | xargs -n 1 -I'{}' jarsigner -keystore $KEYSTORE -storepass $PASS '{}' myself
 
