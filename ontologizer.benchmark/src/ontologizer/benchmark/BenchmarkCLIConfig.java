dc6ca2038888a0ec790ff6fb2b4547af2c8a5d10
Sebastian Bauer <mail@sebastianbauer.info>
2014-02-25 18:28:33 +0100

    Use obo and association parameters.

--- ontologizer.benchmark/src/ontologizer/benchmark/BenchmarkCLIConfig.java
+++ ontologizer.benchmark/src/ontologizer/benchmark/BenchmarkCLIConfig.java
@@ -35,10 +35,12 @@ public class BenchmarkCLIConfig
 	@Parameter(names={"--help"},description="Shows this help.",help=true)
 	public boolean help;
 
-	@Parameter(names={"-o", "--obo"}, description="The obo file that shall be used for running the benchmark", arity=1, required=true)
+	@Parameter(names={"-o", "--obo"}, description="The obo file that shall be used for running the benchmark. For instance, " +
+				"\"http://www.geneontology.org/ontology/gene_ontology_edit.obo\"", arity=1, required=true)
 	public String obo;
 
-	@Parameter(names={"-a", "--association"}, description="Name of the file containing associations from items to terms.", arity=1, required=true)
+	@Parameter(names={"-a", "--association"}, description="Name of the file containing associations from items to terms. For instance, "+
+				"\"http://cvsweb.geneontology.org/cgi-bin/cvsweb.cgi/go/gene-associations/gene_association.fb.gz?rev=HEAD\"", arity=1, required=true)
 	public String assoc;
 
 	@Parameter(names={"--proxy"}, description="Name of the proxy that shall be used for http connections.", arity=1)
@@ -46,5 +48,4 @@ public class BenchmarkCLIConfig
 
 	@Parameter(names={"--proxyPort"}, description="Port of the proxy that shall be used for http connections.", arity=1, validateWith=ProperPositiveInteger.class)
 	public int proxyPort;
-
 }
