9f4233205a2a14835e3e3523919c9aea9755b502
Sebastian Bauer <mail@sebastianbauer.info>
2016-01-28 14:31:02 +0100

    Use latest TeaVM.

--- ontologizer.web/src/main/java/ontologizer/OntologizerClient.java
+++ ontologizer.web/src/main/java/ontologizer/OntologizerClient.java
@@ -1,19 +1,17 @@
 package ontologizer;
 
-import org.teavm.dom.browser.Window;
-import org.teavm.dom.html.HTMLBodyElement;
-import org.teavm.dom.html.HTMLDocument;
-import org.teavm.dom.html.HTMLHeadElement;
-import org.teavm.jso.JS;
+import org.teavm.jso.browser.Window;
+import org.teavm.jso.dom.html.HTMLBodyElement;
+import org.teavm.jso.dom.html.HTMLDocument;
+import org.teavm.jso.dom.html.HTMLHeadElement;
 
 public class OntologizerClient
 {
-	private static Window window = (Window)JS.getGlobal();
-	private static HTMLDocument document = window.getDocument();
+	private static HTMLDocument document = Window.current().getDocument();
 
 	public static void main(String[] args)
 	{
-		HTMLHeadElement head = (HTMLHeadElement) document.createElement("h1");
+		HTMLHeadElement head = document.createElement("h1").cast();
 		head.setInnerHTML("Ontologizer");
 		HTMLBodyElement body = document.getBody();
 		body.appendChild(head);
