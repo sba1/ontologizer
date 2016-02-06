3025e4b5cb2e15c5e6fc413bce4565eb0394d8f1
Sebastian Bauer <mail@sebastianbauer.info>
2016-02-06 23:09:40 +0100

    Extracted writeTable() with OutputStream argument.

--- ontologizer/src/ontologizer/calculation/EnrichedGOTermsTableWriter.java
+++ ontologizer/src/ontologizer/calculation/EnrichedGOTermsTableWriter.java
@@ -1,7 +1,9 @@
 package ontologizer.calculation;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,38 +20,50 @@ public class EnrichedGOTermsTableWriter
 	private static Logger logger = Logger.getLogger(EnrichedGOTermsResult.class.getName());
 
 	/**
-	 * Write the results as tab-separated file.
+	 * Write the results in a tab-separated format to the given output stream.
 	 *
-	 * @param file
+	 * @param os the stream in which the data is written to.
+	 * @param result the results to save
 	 */
-	public static void writeTable(File file, EnrichedGOTermsResult result)
+	public static void writeTable(OutputStream os, EnrichedGOTermsResult result)
 	{
 		AbstractGOTermProperties first = result.iterator().next();
 
+		PrintWriter out = new PrintWriter(os);
+
+		/* Write out the table header */
+
+		out.write(first.propHeaderToString());
+
+		/* Place the result into an own list, so we can sort the results */
+		ArrayList<AbstractGOTermProperties> propsList = new ArrayList<AbstractGOTermProperties>();
+		for (AbstractGOTermProperties props : result)
+			propsList.add(props);
+		Collections.sort(propsList);
+
+		/* Write out table contents */
+		for (AbstractGOTermProperties props : propsList)
+		{
+			out.println(props.propLineToString(result.getPopulationGeneCount(), result.getStudyGeneCount()));
+		}
+
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * Write the results as tab-separated file.
+	 *
+	 * @param file the filename that is used for the newly created file.
+	 * @param result the results to save
+	 */
+	public static void writeTable(File file, EnrichedGOTermsResult result)
+	{
 		try
 		{
 			logger.log(Level.INFO, "Writing to \"" + file.getCanonicalPath() + "\".");
 
-			PrintWriter out = new PrintWriter(file);
-
-			/* Write out the table header */
-
-			out.write(first.propHeaderToString());
-
-			/* Place the result into an own list, so we can sort the results */
-			ArrayList<AbstractGOTermProperties> propsList = new ArrayList<AbstractGOTermProperties>();
-			for (AbstractGOTermProperties props : result)
-				propsList.add(props);
-			Collections.sort(propsList);
-
-			/* Write out table contents */
-			for (AbstractGOTermProperties props : propsList)
-			{
-				out.println(props.propLineToString(result.getPopulationGeneCount(), result.getStudyGeneCount()));
-			}
-
-			out.flush();
-			out.close();
+			writeTable(new FileOutputStream(file), result);
 
 			logger.log(Level.INFO, "\"" + file.getCanonicalPath() + "\"" + " successfully written.");
 		} catch (IOException e)
