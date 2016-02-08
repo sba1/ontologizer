093e59ae2f8ef6021dc6e60ea9e58c6495064bd7
Sebastian Bauer <mail@sebastianbauer.info>
2016-02-08 23:26:38 +0100

    Introduced dataprep module.

---
+++ ontologizer.dataprep/src/main/java/org/ontologizer/dataprep/DataPrep.java
@@ -0,0 +1,13 @@
+package org.ontologizer.dataprep;
+
+/**
+ * A simple tool preparing data for (Web) ontologizer.
+ *
+ * @author Sebastian Bauer
+ */
+public class DataPrep
+{
+	public static void main(String[] args)
+	{
+	}
+}
