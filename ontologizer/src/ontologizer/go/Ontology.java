0419f6c951145deb7d21540c3a4a0e28afe0d54a
Sebastian Bauer <mail@sebastianbauer.info>
2016-02-09 21:23:59 +0100

    Removed unneeded import.

--- ontologizer/src/ontologizer/go/Ontology.java
+++ ontologizer/src/ontologizer/go/Ontology.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
