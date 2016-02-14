686763db56e7c945846c5810c78589cf47f55717
Sebastian Bauer <mail@sebastianbauer.info>
2016-02-14 20:09:25 +0100

    Delegate getIDAsString().

--- ontologizer/src/ontologizer/go/Term.java
+++ ontologizer/src/ontologizer/go/Term.java
@@ -192,28 +192,13 @@ public class Term {
 	}
 
 	/**
-	 * Returns the Term ID as a string (uses StringBuilder)
+	 * Returns the id of the term as a string.
 	 *
-	 * @return term:id
+	 * @return the term id as string.
 	 */
-	public String getIDAsString() {
-		StringBuilder buffer = new StringBuilder();
-		buffer.append(id.getPrefix());
-		buffer.append(":");
-		if (id.id < 10)
-			buffer.append("000000");
-		else if (id.id < 100)
-			buffer.append("00000");
-		else if (id.id < 1000)
-			buffer.append("0000");
-		else if (id.id < 10000)
-			buffer.append("000");
-		else if (id.id < 100000)
-			buffer.append("00");
-		else if (id.id < 1000000)
-			buffer.append("0");
-		buffer.append(id.id);
-		return buffer.toString();
+	public String getIDAsString()
+	{
+		return id.toString();
 	}
 
 	/**
