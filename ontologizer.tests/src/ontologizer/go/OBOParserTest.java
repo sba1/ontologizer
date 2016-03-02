7f206504646d881ec8821163ec30132e74f1d921
Sebastian Bauer <mail@sebastianbauer.info>
2016-03-02 20:39:34 +0100

    Check for proper subset name.

--- ontologizer.tests/src/ontologizer/go/OBOParserTest.java
+++ ontologizer.tests/src/ontologizer/go/OBOParserTest.java
@@ -313,6 +313,7 @@ public class OBOParserTest
 		ArrayList<Term> terms = new ArrayList<Term>(oboParser.getTermMap());
 		assertEquals(2, terms.size());
 		assertEquals(1, terms.get(0).getSubsets().length);
+		assertEquals("subset", terms.get(0).getSubsets()[0].getName());
 		assertEquals(0, terms.get(1).getSubsets().length);
 	}
 
