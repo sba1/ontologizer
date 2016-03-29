2834dc5553b7e8802a5c79ef4f7e43aa899a7034
Sebastian Bauer <mail@sebastianbauer.info>
2016-03-29 22:23:30 +0200

    Use more recent BareBonesBrowserLaunch class.

--- ontologizer/src/ontologizer/util/BareBonesBrowserLaunch.java
+++ ontologizer/src/ontologizer/util/BareBonesBrowserLaunch.java
@@ -2,50 +2,66 @@ package ontologizer.util;
 
 /////////////////////////////////////////////////////////
 //Bare Bones Browser Launch                          //
-//Version 1.5                                        //
-//December 10, 2005                                  //
-//Supports: Mac OS X, GNU/Linux, Unix, Windows XP    //
+//Version 3.2 (October 24, 2010)                     //
+//By Dem Pilafian                                    //
+//Supports:                                          //
+//Mac OS X, GNU/Linux, Unix, Windows XP/Vista/7   //
 //Example Usage:                                     //
-// String url = "http://www.centerkey.com/";       //
-// BareBonesBrowserLaunch.openURL(url);            //
+//String url = "http://centerkey.com/";       //
+//BareBonesBrowserLaunch.openURL(url);            //
 //Public Domain Software -- Free to Use as You Like  //
 /////////////////////////////////////////////////////////
 
-import java.lang.reflect.Method;
 import javax.swing.JOptionPane;
+import java.util.Arrays;
 
-public class BareBonesBrowserLaunch {
+public class BareBonesBrowserLaunch
+{
+	static final String[] browsers = { "x-www-browser", "google-chrome",
+			"firefox", "opera", "epiphany", "konqueror", "conkeror", "midori",
+			"kazehakase", "mozilla" };
+	static final String errMsg = "Error attempting to launch web browser";
 
-private static final String errMsg = "Error attempting to launch web browser";
-
-public static void openURL(String url) {
-  String osName = System.getProperty("os.name");
-  try {
-     if (osName.startsWith("Mac OS")) {
-        Class fileMgr = Class.forName("com.apple.eio.FileManager");
-        Method openURL = fileMgr.getDeclaredMethod("openURL",
-           new Class[] {String.class});
-        openURL.invoke(null, new Object[] {url});
-        }
-     else if (osName.startsWith("Windows"))
-        Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
-     else { //assume Unix or Linux
-        String[] browsers = {
-           "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
-        String browser = null;
-        for (int count = 0; count < browsers.length && browser == null; count++)
-           if (Runtime.getRuntime().exec(
-                 new String[] {"which", browsers[count]}).waitFor() == 0)
-              browser = browsers[count];
-        if (browser == null)
-           throw new Exception("Could not find web browser");
-        else
-           Runtime.getRuntime().exec(new String[] {browser, url});
-        }
-     }
-  catch (Exception e) {
-     JOptionPane.showMessageDialog(null, errMsg + ":\n" + e.getLocalizedMessage());
-     }
-  }
-
+	public static void openURL(String url)
+	{
+		try
+		{ // attempt to use Desktop library from JDK 1.6+
+			Class<?> d = Class.forName("java.awt.Desktop");
+			d.getDeclaredMethod("browse", new Class[] { java.net.URI.class })
+					.invoke(d.getDeclaredMethod("getDesktop").invoke(null),
+							new Object[] { java.net.URI.create(url) });
+			// above code mimicks: java.awt.Desktop.getDesktop().browse()
+		} catch (Exception ignore)
+		{ // library not available or failed
+			String osName = System.getProperty("os.name");
+			try
+			{
+				if (osName.startsWith("Mac OS"))
+				{
+					Class.forName("com.apple.eio.FileManager")
+							.getDeclaredMethod("openURL",
+									new Class[] { String.class })
+							.invoke(null, new Object[] { url });
+				} else if (osName.startsWith("Windows"))
+					Runtime.getRuntime().exec(
+							"rundll32 url.dll,FileProtocolHandler " + url);
+				else
+				{ // assume Unix or Linux
+					String browser = null;
+					for (String b : browsers)
+						if (browser == null && Runtime.getRuntime()
+								.exec(new String[] { "which", b })
+								.getInputStream().read() != -1)
+							Runtime.getRuntime()
+									.exec(new String[] { browser = b, url });
+					if (browser == null)
+						throw new Exception(Arrays.toString(browsers));
+				}
+			} catch (Exception e)
+			{
+				JOptionPane.showMessageDialog(null,
+						errMsg + "\n" + e.toString());
+			}
+		}
+	}
 }
