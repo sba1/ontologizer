cd87572095d4d5584c4015b99b940e3f059a2944
Sebastian Bauer <mail@sebastianbauer.info>
2016-06-04 07:30:48 +0200

    Extracted performTestCalculation().

--- ontologizer.tests/src/ontologizer/calculation/CalculationTestUtils.java
+++ ontologizer.tests/src/ontologizer/calculation/CalculationTestUtils.java
@@ -1,6 +1,15 @@
 package ontologizer.calculation;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Random;
+
+import ontologizer.association.AssociationContainer;
+import ontologizer.internal.InternalOntology;
+import ontologizer.ontology.Ontology;
 import ontologizer.ontology.TermID;
+import ontologizer.statistics.None;
 
 public class CalculationTestUtils
 {
@@ -8,4 +17,34 @@ public class CalculationTestUtils
 	{
 		return (TermForTermGOTermProperties)result.getGOTermProperties(new TermID(id));
 	}
+
+	/**
+	 * Perform a standard test calculation on the initial ontology
+	 *
+	 * @return
+	 */
+	public static EnrichedGOTermsResult performTestCalculation()
+	{
+		InternalOntology internalOntology = new InternalOntology();
+
+		final HashMap<TermID,Double> wantedActiveTerms = new HashMap<TermID,Double>(); /* Terms that are active */
+		wantedActiveTerms.put(new TermID("GO:0000004"),0.0);
+
+		AssociationContainer assoc = internalOntology.assoc;
+		Ontology ontology = internalOntology.graph;
+
+		SingleCalculationSetting scs = SingleCalculationSetting.create(new Random(1), wantedActiveTerms, 0.00, ontology, assoc);
+		assertEquals(500, scs.pop.getGeneCount());
+		assertEquals(57, scs.study.getGeneCount());
+
+		TermForTermCalculation tft = new TermForTermCalculation();
+		EnrichedGOTermsResult r = tft.calculateStudySet(ontology, assoc, scs.pop, scs.study, new None());
+		EnrichedGOTermsTableWriter.writeTable(System.out, r);
+
+		assertEquals(11, r.getSize());
+		assertEquals(500, r.getPopulationGeneCount());
+		assertEquals(57, r.getStudyGeneCount());
+		return r;
+	}
+
 }
--- ontologizer.tests/src/ontologizer/calculation/TermForTermCalculationTest.java
+++ ontologizer.tests/src/ontologizer/calculation/TermForTermCalculationTest.java
@@ -1,18 +1,10 @@
 package ontologizer.calculation;
 
+import static ontologizer.calculation.CalculationTestUtils.performTestCalculation;
 import static ontologizer.calculation.CalculationTestUtils.prop;
 import static org.junit.Assert.assertEquals;
 
-import java.util.HashMap;
-import java.util.Random;
-
 import org.junit.Test;
-
-import ontologizer.association.AssociationContainer;
-import ontologizer.internal.InternalOntology;
-import ontologizer.ontology.Ontology;
-import ontologizer.ontology.TermID;
-import ontologizer.statistics.None;
 
 public class TermForTermCalculationTest
 {
@@ -31,25 +23,7 @@ public class TermForTermCalculationTest
 				this.p = p;
 			}
 		}
-		InternalOntology internalOntology = new InternalOntology();
-
-		final HashMap<TermID,Double> wantedActiveTerms = new HashMap<TermID,Double>(); /* Terms that are active */
-		wantedActiveTerms.put(new TermID("GO:0000004"),0.0);
-
-		AssociationContainer assoc = internalOntology.assoc;
-		Ontology ontology = internalOntology.graph;
-
-		SingleCalculationSetting scs = SingleCalculationSetting.create(new Random(1), wantedActiveTerms, 0.00, ontology, assoc);
-		assertEquals(500, scs.pop.getGeneCount());
-		assertEquals(57, scs.study.getGeneCount());
-
-		TermForTermCalculation tft = new TermForTermCalculation();
-		EnrichedGOTermsResult r = tft.calculateStudySet(ontology, assoc, scs.pop, scs.study, new None());
-		EnrichedGOTermsTableWriter.writeTable(System.out, r);
-
-		assertEquals(11, r.getSize());
-		assertEquals(500, r.getPopulationGeneCount());
-		assertEquals(57, r.getStudyGeneCount());
+		EnrichedGOTermsResult r = performTestCalculation();
 
 		TermForTermGOTermProperties [] p = new TermForTermGOTermProperties[r.getSize()];
 		p[0] = prop(r, "GO:0000001");
