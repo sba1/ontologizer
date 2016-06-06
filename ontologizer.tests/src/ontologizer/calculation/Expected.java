77b91e50e24e7639a596714cb5b4dea98f52cf71
Sebastian Bauer <mail@sebastianbauer.info>
2016-06-06 20:47:44 +0200

    Extracted Expected class.

---
+++ ontologizer.tests/src/ontologizer/calculation/Expected.java
@@ -0,0 +1,16 @@
+package ontologizer.calculation;
+
+class Expected
+{
+	public String id;
+	public int pop;
+	public int study;
+	public double p;
+	public Expected(String id, int pop, int study, double p)
+	{
+		this.id = id;
+		this.pop = pop;
+		this.study = study;
+		this.p = p;
+	}
+}
--- ontologizer.tests/src/ontologizer/calculation/TermForTermCalculationTest.java
+++ ontologizer.tests/src/ontologizer/calculation/TermForTermCalculationTest.java
@@ -12,20 +12,6 @@ public class TermForTermCalculationTest
 	@Test
 	public void whetherTFTWorks()
 	{
-		class Expected
-		{
-			public String id;
-			public int pop;
-			public int study;
-			public double p;
-			public Expected(String id, int pop, int study, double p)
-			{
-				this.id = id;
-				this.pop = pop;
-				this.study = study;
-				this.p = p;
-			}
-		}
 		EnrichedGOTermsResult r = performTestCalculation(new TermForTermCalculation());
 
 		Expected [] expected = new Expected[]
