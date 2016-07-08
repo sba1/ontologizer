806672d5532093e0e2c372c20d4176c38bf430ac
Sebastian Bauer <mail@sebastianbauer.info>
2016-07-08 17:07:15 +0200

    Added preliminary workspace class.

---
+++ ontologizer.gui/src/ontologizer/gui/swt/Workspace.java
@@ -0,0 +1,50 @@
+package ontologizer.gui.swt;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Workspace
+{
+	private File defaultDirectory;
+
+	private List<Project> projects = new ArrayList<Project>();
+
+	public Workspace(File defaultDirectory)
+	{
+		setDefaultDirectory(defaultDirectory);
+	}
+
+	public void setDefaultDirectory(File defaultDirectory)
+	{
+		if (!defaultDirectory.isDirectory())
+			throw new IllegalArgumentException();
+
+		this.defaultDirectory = defaultDirectory;
+
+		refresh();
+	}
+
+	private void refresh()
+	{
+		String [] projectNames = defaultDirectory.list(new FilenameFilter()
+		{
+			public boolean accept(File dir, String name)
+			{
+				if (name.startsWith(".")) return false;
+				return true;
+			}
+		});
+		Arrays.sort(projectNames,String.CASE_INSENSITIVE_ORDER);
+
+		for (String project : projectNames)
+		{
+			if (project.equals(".cache"))
+				continue;
+
+			projects.add(new Project(new File(defaultDirectory, project)));
+		}
+	}
+}
