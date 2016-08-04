7353071d0887599d2b10327975aeddf5d35a5f09
Sebastian Bauer <mail@sebastianbauer.info>
2016-08-04 07:25:20 +0200

    Added Javadoc.

--- ontologizer.web.common/src/main/java/ontologizer/WorkerMessageReceiver.java
+++ ontologizer.web.common/src/main/java/ontologizer/WorkerMessageReceiver.java
@@ -1,5 +1,12 @@
 package ontologizer;
 
+/**
+ * Interface for receiving/handling worker messages.
+ *
+ * @author Sebastian Bauer
+ *
+ * @param <T> type of the worker message that will be handled.
+ */
 public interface WorkerMessageReceiver<T extends WorkerMessage>
 {
 	void receive(T msg);
