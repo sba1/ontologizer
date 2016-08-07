46be97e788ddd9fccf2030f7965386334cec2fd4
Sebastian Bauer <mail@sebastianbauer.info>
2016-08-07 22:41:07 +0200

    Added few Javadocs.

--- ontologizer.web.common/src/main/java/ontologizer/IWhenDone.java
+++ ontologizer.web.common/src/main/java/ontologizer/IWhenDone.java
@@ -1,5 +1,13 @@
 package ontologizer;
 
+/**
+ * Simple interface that is used for in the context of the poster of a
+ * replyable message to implement the retrieval of the result.
+ *
+ * @author Sebastian Bauer
+ *
+ * @param <R> the result type
+ */
 public interface IWhenDone<R>
 {
 	public void done(R result);
--- ontologizer.web.common/src/main/java/ontologizer/ReplyableWorkerMessage.java
+++ ontologizer.web.common/src/main/java/ontologizer/ReplyableWorkerMessage.java
@@ -3,6 +3,13 @@ package ontologizer;
 import org.teavm.jso.JSObject;
 import org.teavm.jso.JSProperty;
 
+/**
+ * A replyable worker message is a message from a poster who also expect a result.
+ *
+ * @author Sebastian Bauer
+ *
+ * @param <R> the type of the result
+ */
 public abstract class ReplyableWorkerMessage<R extends JSObject> extends WorkerMessage
 {
 	@JSProperty
