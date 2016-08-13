e07438a02e130c7d75ed1d711c88e7f6c39a9a53
Sebastian Bauer <mail@sebastianbauer.info>
2016-08-13 07:54:18 +0200

    Skip source jar generation when building the Web version.

--- build-web.sh
+++ build-web.sh
@@ -1,3 +1,3 @@
 #! /bin/bash -x
 
-(cd ontologizer.parent && mvn -P Web -DskipTests package -Dmaven.javadoc.skip=true -pl ontologizer:ontologizer.web -am)
+(cd ontologizer.parent && mvn -P Web -DskipSource -DskipTests package -Dmaven.javadoc.skip=true -pl ontologizer:ontologizer.web -am)
