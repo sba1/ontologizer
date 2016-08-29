87bffc0f9888eb7b2ee7577a873d3b70fe5ab47c
Sebastian Bauer <mail@sebastianbauer.info>
2016-08-29 20:25:47 +0200

    Expand imports.

--- ontologizer/src/ontologizer/association/AssociationContainer.java
+++ ontologizer/src/ontologizer/association/AssociationContainer.java
@@ -1,6 +1,10 @@
 package ontologizer.association;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import ontologizer.types.ByteString;
 
