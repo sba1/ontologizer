cd9b4a3346c6b67f29bfe90cdee3d072b21cfdf6
Sebastian Bauer <mail@sebastianbauer.info>
2016-10-05 22:13:01 +0200

    Use static imports.

--- ontologizer.tests/src/ontologizer/util/UtilTest.java
+++ ontologizer.tests/src/ontologizer/util/UtilTest.java
@@ -1,6 +1,8 @@
 package ontologizer.util;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class UtilTest
@@ -13,9 +15,9 @@ public class UtilTest
 		int [] c = new int[]{4,5,6,7};
 		int [] d = new int[]{7,8,9};
 
-		Assert.assertEquals(2, Util.commonInts(a, b));
-		Assert.assertEquals(3, Util.commonInts(a, c));
-		Assert.assertEquals(0, Util.commonInts(a, d));
+		assertEquals(2, Util.commonInts(a, b));
+		assertEquals(3, Util.commonInts(a, c));
+		assertEquals(0, Util.commonInts(a, d));
 	}
 
 	@Test
@@ -26,20 +28,20 @@ public class UtilTest
 		int [] c = new int[]{4,5,6,7};
 		int [] d = new int[]{7,8,9};
 
-		Assert.assertArrayEquals(a, Util.union(a, a));
-		Assert.assertArrayEquals(b, Util.union(b, b));
-		Assert.assertArrayEquals(c, Util.union(c, c));
-		Assert.assertArrayEquals(d, Util.union(d, d));
+		assertArrayEquals(a, Util.union(a, a));
+		assertArrayEquals(b, Util.union(b, b));
+		assertArrayEquals(c, Util.union(c, c));
+		assertArrayEquals(d, Util.union(d, d));
 
-		Assert.assertArrayEquals(a, Util.union(new int[]{}, a));
-		Assert.assertArrayEquals(a, Util.union(a, new int[]{}));
-		Assert.assertArrayEquals(a, Util.union(a, b));
+		assertArrayEquals(a, Util.union(new int[]{}, a));
+		assertArrayEquals(a, Util.union(a, new int[]{}));
+		assertArrayEquals(a, Util.union(a, b));
 
-		Assert.assertArrayEquals(new int[]{1,2,3,4,5,6,7}, Util.union(a, c));
-		Assert.assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9}, Util.union(a, d));
+		assertArrayEquals(new int[]{1,2,3,4,5,6,7}, Util.union(a, c));
+		assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9}, Util.union(a, d));
 
-		Assert.assertArrayEquals(new int[]{4,5,6,7}, Util.union(b, c));
+		assertArrayEquals(new int[]{4,5,6,7}, Util.union(b, c));
 
-		Assert.assertArrayEquals(new int[]{4,5,6,7,8,9}, Util.union(c, d));
+		assertArrayEquals(new int[]{4,5,6,7,8,9}, Util.union(c, d));
 	}
 }
