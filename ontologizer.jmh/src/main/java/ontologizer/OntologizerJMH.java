183ab18f77ffe16cd92102527692b2941cfea1b1
Sebastian Bauer <mail@sebastianbauer.info>
2016-11-14 21:54:53 +0100

    Added not-so-micro benchmark for parsing obo files.

--- ontologizer.jmh/src/main/java/ontologizer/OntologizerJMH.java
+++ ontologizer.jmh/src/main/java/ontologizer/OntologizerJMH.java
@@ -1,11 +1,21 @@
 package ontologizer;
 
+import java.io.IOException;
+
 import org.openjdk.jmh.annotations.Benchmark;
+
+import ontologizer.ontology.OBOParser;
+import ontologizer.ontology.OBOParserException;
+import ontologizer.ontology.OBOParserFileInput;
 
 public class OntologizerJMH
 {
+	private static final String obofile = "../../ontologizer.tests/data/gene_ontology.1_2.obo.gz";
+
 	@Benchmark
-	public void testMethod()
+	public void benchmarkOBOParser() throws IOException, OBOParserException
 	{
+		OBOParser oboParser = new OBOParser(new OBOParserFileInput(obofile));
+		oboParser.doParse();
 	}
 }
