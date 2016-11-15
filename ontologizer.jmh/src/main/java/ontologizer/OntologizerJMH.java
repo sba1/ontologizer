ad5963950878503320154c34dedc95888a01f197
Sebastian Bauer <mail@sebastianbauer.info>
2016-11-15 22:04:18 +0100

    Do the benchmark less often.

--- ontologizer.jmh/src/main/java/ontologizer/OntologizerJMH.java
+++ ontologizer.jmh/src/main/java/ontologizer/OntologizerJMH.java
@@ -1,8 +1,12 @@
 package ontologizer;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Warmup;
 
 import ontologizer.ontology.OBOParser;
 import ontologizer.ontology.OBOParserException;
@@ -13,6 +17,9 @@ public class OntologizerJMH
 	private static final String obofile = "../../ontologizer.tests/data/gene_ontology.1_2.obo.gz";
 
 	@Benchmark
+	@Warmup(iterations=5)
+	@Fork(value=1)
+	@Measurement(time=2,timeUnit=TimeUnit.SECONDS)
 	public void benchmarkOBOParser() throws IOException, OBOParserException
 	{
 		OBOParser oboParser = new OBOParser(new OBOParserFileInput(obofile));
