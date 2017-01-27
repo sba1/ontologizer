f1306330a25e7254808a1efb60b001a102671419
Sebastian Bauer <mail@sebastianbauer.info>
2017-01-27 20:31:59 +0100

    Fixed and enhanced Javadocs.

--- ontologizer/src/ontologizer/ontology/ParentTermID.java
+++ ontologizer/src/ontologizer/ontology/ParentTermID.java
@@ -2,13 +2,18 @@ package ontologizer.ontology;
 
 /**
  * This class is used to specify a parent accompanied with
- * the kind of relationship.
+ * the kind of relationship. Note that the term "parent" is
+ * indeed a misnomer. The usage of this term has historical
+ * reasons when there were only a limited set of relations.
  *
- * @author sba
+ * @author Sebastian Bauer
  */
 public class ParentTermID
 {
+	/** The id of the related term */
 	public TermID termid;
+
+	/** the type of the relation */
 	public TermRelation relation;
 
 	public ParentTermID(TermID parent, TermRelation relation)
