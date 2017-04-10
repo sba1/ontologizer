74d865bd273ddefef921c7b4fc76d2f342f2472e
Sebastian Bauer <mail@sebastianbauer.info>
2017-04-10 20:50:07 +0200

    Improved test.

--- ontologizer.tests/src/ontologizer/calculation/AnnontationContextTest.java
+++ ontologizer.tests/src/ontologizer/calculation/AnnontationContextTest.java
@@ -28,7 +28,12 @@ public class AnnontationContextTest
 		ArrayList<Association> associations = new ArrayList<Association>();
 		Prefix goPrefix = new Prefix("GO");
 
-		associations.add(new Association(b("item1"), new TermID(goPrefix, 1)));
+		TermID t1 = new TermID(goPrefix, 1);
+		TermID t2 = new TermID(goPrefix, 2);
+		TermID t3 = new TermID(goPrefix, 3);
+		associations.add(new Association(b("item1"), t1));
+		associations.add(new Association(b("item2"), t2));
+		associations.add(new Association(b("item3"), t3));
 		ArrayList<ByteString> symbols = new ArrayList<ByteString>(associations.size());
 		for (Association a : associations)
 		{
@@ -39,8 +44,14 @@ public class AnnontationContextTest
 
 		AssociationContainer assoc = new AssociationContainer(associations, ac);
 		CalculationContext cc = CalculationContext.create(g, assoc);
-		Assert.assertEquals(1, cc.item2Terms.length);
-		Assert.assertEquals(1, cc.item2Terms[0].length);
-		Assert.assertEquals(g.getVertexIndex(new TermID(goPrefix, 1)), cc.item2Terms[0][0]);
-	}
+		Assert.assertEquals(3, cc.item2Terms.length);
+
+		int i1 = ac.mapSymbol(b("item1"));
+		Assert.assertEquals(1, cc.item2Terms[i1].length);
+		Assert.assertEquals(g.getVertexIndex(t1), cc.item2Terms[i1][0]);
+
+		Assert.assertEquals(2, cc.item2Terms[ac.mapSymbol(b("item2"))].length);
+
+		Assert.assertEquals(2, cc.item2Terms[ac.mapSymbol(b("item3"))].length);
+}
 }
