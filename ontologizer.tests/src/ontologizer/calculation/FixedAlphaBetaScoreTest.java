4e474abb35a41d8a3b7e87b7244fc57e642feb78
Sebastian Bauer <mail@sebastianbauer.info>
2017-04-22 10:44:22 +0200

    Use tid().

--- ontologizer.tests/src/ontologizer/calculation/FixedAlphaBetaScoreTest.java
+++ ontologizer.tests/src/ontologizer/calculation/FixedAlphaBetaScoreTest.java
@@ -29,7 +29,7 @@ public class FixedAlphaBetaScoreTest
 	{
 		List<TermID> list = new LinkedList<TermID>();
 		for (String t : termIDs)
-			list.add(new TermID(t));
+			list.add(tid(t));
 		return list;
 	}
 
