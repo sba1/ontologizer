ac4ba38c4de2da95717bf1a63ee7a41b79a142e0
Sebastian Bauer <mail@sebastianbauer.info>
2017-05-02 20:31:31 +0200

    Removed a single System.out.println() call.

--- ontologizer.tests/src/ontologizer/calculation/Bayes2GOCalculationTest.java
+++ ontologizer.tests/src/ontologizer/calculation/Bayes2GOCalculationTest.java
@@ -140,7 +140,6 @@ public class Bayes2GOCalculationTest
 		IntMapper<ByteString> geneMapper = IntMapper.create(populationEnumerator.getGenesAsList());
 		int [][] termLinks = CalculationUtils.makeTermLinks(populationEnumerator, termMapper, geneMapper);
 
-		System.out.println(am.getSymbols().length);
 		double [] result = calc.calculate(termLinks, studyIds, am.getSymbols().length);
 		assertEquals(termLinks.length, result.length);
 	}
