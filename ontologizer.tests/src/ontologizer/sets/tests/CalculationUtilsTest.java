c0a9504bf74491ae63389b08c1d7bdc990cfbca1
Sebastian Bauer <mail@sebastianbauer.info>
2018-03-15 20:18:02 +0100

    Add test for mixed names.

--- ontologizer.tests/src/ontologizer/sets/tests/CalculationUtilsTest.java
+++ ontologizer.tests/src/ontologizer/sets/tests/CalculationUtilsTest.java
@@ -59,4 +59,32 @@ public class CalculationUtilsTest
 			assertEquals(i, ids[i]);
 		}
 	}
+
+	@Test
+	public void testGetUniqueIDsWithMixedSynonyms()
+	{
+		InternalDatafiles idf = new InternalDatafiles();
+		AnnotationContext annotationContext = idf.assoc.getMapping();
+
+		StudySet studySet = new StudySet();
+		studySet.addGene(b("item1"), "");
+		studySet.addGene(b("gene1"), "");
+		studySet.addGene(b("item2"), "");
+		studySet.addGene(b("gene2"), "");
+		studySet.addGene(b("item3"), "");
+		studySet.addGene(b("gene3"), "");
+		studySet.addGene(b("item4"), "");
+		studySet.addGene(b("gene4"), "");
+		studySet.addGene(b("item5"), "");
+		studySet.addGene(b("gene5"), "");
+
+		IntMapper<ByteString> mapper = IntMapper.create(studySet, studySet.getGeneCount());
+
+		int [] ids = CalculationUtils.getUniqueIDs(studySet, studySet.getGeneCount(), mapper, annotationContext);
+
+		for (int i = 0; i < ids.length; i++)
+		{
+			assertEquals(i, ids[i]);
+		}
+	}
 }
