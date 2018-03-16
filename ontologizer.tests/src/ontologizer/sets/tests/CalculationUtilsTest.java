3663c882e02511854332c16ae3cb5f7b1de177d9
Sebastian Bauer <mail@sebastianbauer.info>
2018-03-16 15:10:13 +0100

    Test synonyms properly.

--- ontologizer.tests/src/ontologizer/sets/tests/CalculationUtilsTest.java
+++ ontologizer.tests/src/ontologizer/sets/tests/CalculationUtilsTest.java
@@ -2,6 +2,9 @@ package ontologizer.sets.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import ontologizer.association.AnnotationContext;
@@ -68,19 +71,23 @@ public class CalculationUtilsTest
 
 		StudySet studySet = new StudySet();
 		studySet.addGene(b("item1"), "");
-		studySet.addGene(b("gene1"), "");
 		studySet.addGene(b("item2"), "");
-		studySet.addGene(b("gene2"), "");
 		studySet.addGene(b("item3"), "");
-		studySet.addGene(b("gene3"), "");
 		studySet.addGene(b("item4"), "");
-		studySet.addGene(b("gene4"), "");
 		studySet.addGene(b("item5"), "");
-		studySet.addGene(b("gene5"), "");
 
 		IntMapper<ByteString> mapper = IntMapper.create(studySet, studySet.getGeneCount());
 
+		List<ByteString> genes = new ArrayList<ByteString>();
+		genes.add(b("gene1"));
+		genes.add(b("item1"));
+		genes.add(b("item2"));
+		genes.add(b("gene3"));
+		genes.add(b("item4"));
+		genes.add(b("gene5"));
+
 		int [] ids = CalculationUtils.getUniqueIDs(studySet, studySet.getGeneCount(), mapper, annotationContext);
+		assertEquals(5, ids.length);
 
 		for (int i = 0; i < ids.length; i++)
 		{
