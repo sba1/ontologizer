62765f4e5d42ddaae999c480e7ef50bb510f5fc8
Sebastian Bauer <mail@sebastianbauer.info>
2018-03-17 21:13:17 +0100

    Document this class.

--- ontologizer.tests/src/ontologizer/calculation/Expected.java
+++ ontologizer.tests/src/ontologizer/calculation/Expected.java
@@ -1,11 +1,25 @@
 package ontologizer.calculation;
 
+/**
+ * Helper class for keeping some expected values
+ * in a more structured way.
+ *
+ * @author Sebastian Bauer
+ */
 class Expected
 {
+	/** Term of id */
 	public String id;
+
+	/** Number of genes annotated to this term in the population */
 	public int pop;
+
+	/** Number of genes annotated to this term in the study set */
 	public int study;
+
+	/** The probability */
 	public double p;
+
 	public Expected(String id, int pop, int study, double p)
 	{
 		this.id = id;
